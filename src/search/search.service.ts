06c4b3670d1da7c9f60419050801ae38e7b50990
Phuc Le <lenguyenminhphuc0706@gmail.com>
2025-09-07 17:04:32 +0700

    format code

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -4,38 +4,19 @@ import { ElasticsearchService } from '@nestjs/elasticsearch';
 @Injectable()
 export class SearchService {
   constructor(private readonly esService: ElasticsearchService) {}
+    async searchPosts(query: string) {
+    const { hits } = await this.esService.search({
+        index: 'posts',
+        body: {
+        query: {
+            match: { content: query },
+        },
+        },
+    });
 
-//   async searchPosts(query: string) {
-//     const { hits } = await this.esService.search({
-//       index: 'posts', // index Elasticsearch
-//       body: {
-//         query: {
-//           match: {
-//             content: query, // tìm kiếm full-text trong field content
-//           },
-//         },
-//       },
-//     });
-
-//     return hits.hits.map((hit: any) => ({
-//       id: hit._id,
-//       ...hit._source,
-//     }));
-//   }
-async searchPosts(query: string) {
-  const { hits } = await this.esService.search({
-    index: 'posts',
-    body: {
-      query: {
-        match: { content: query },
-      },
-    },
-  });
-
-  return hits.hits.map((hit: any) => ({
-    id: hit._id,
-    ...hit._source,
-  })); // phải là array chứ không bọc trong object
-}
-
+    return hits.hits.map((hit: any) => ({
+        id: hit._id,
+        ...hit._source,
+    })); // phải là array chứ không bọc trong object
+    }
 }
